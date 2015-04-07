11bcc016f16a54b94dd5612fac4f82dd538b399f
mdebanjan <mdebanjan@users.noreply.github.com>
2015-04-06 23:09:17 -0700

    Committed by Udacity

---
+++ PitchPerfect/RecordedAudio.swift
@@ -0,0 +1,15 @@
+//
+//  RecordedAudio.swift
+//  PitchPerfect
+//
+//  Created by Mukherjee, Debanjan on 4/6/15.
+//  Copyright (c) 2015 Mukherjee, Debanjan. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    
+    var filePathUrl: NSURL!
+    var title: String!
+}
